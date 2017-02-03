172a37f6f696c52524953354b3f30a64d3b7151a
Steven Buxton <breathesteve@gmail.com>
2017-02-02 18:12:19 -0600

    Added random function logic

--- color.js
+++ color.js
@@ -8,8 +8,9 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll('.square');
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
+var messageDisplay = document.querySelector('#message');
 
 colorDisplay.textContent = pickedColor;
 
@@ -18,9 +19,22 @@ for (var i = 0; i < squares.length; i++) {
 	squares[i].addEventListener('click', function(){
 		var clickedColor = this.style.background;
 		if (clickedColor === pickedColor) {
-			alert('Correct');
+			messageDisplay.textContent = 'Correct!';
+			changeColors(clickedColor);
 		} else {
-			alert('Wrong')
+			this.style.background = '#232323';
+			messageDisplay.textContent = 'Try Again';
 		}
 	});
+}
+
+function changeColors(color) {
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].style.background = color;
+	}
+}
+
+function pickColor() {
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
 }
